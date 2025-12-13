67816abdfcdb245e1307203aa4f0a47f0f995080
YuukiAiN0 <nanthawat.esa@gmail.com>
2025-12-13 09:37:40 +0700

    commit

--- my-resume/vite.config.js
+++ my-resume/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/backend-w02-resume/'  // Replace with your repo name
+  base: '/my-resume/'  // Replace with your repo name
 })
